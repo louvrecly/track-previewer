6d4fe32a8e81426e4e1411755db33d95d943b92e
Louvre Chow <louvrecly@gmail.com>
2023-11-06 19:21:58 -0500

    add audio preview in TrackCard

--- app/track-list/track-card/index.tsx
+++ app/track-list/track-card/index.tsx
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic';
 import { useMemo } from 'react';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
@@ -5,6 +6,8 @@ import CardContent from '@mui/material/CardContent';
 import TrackInfo from './track-info';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
+
+const Box = dynamic(() => import('@mui/material/Box'));
 
 interface TrackCardProp {
   track: SpotifyApi.TrackObjectFull;
@@ -20,30 +23,38 @@ const TrackCard = ({ track }: TrackCardProp) => {
   );
 
   return (
-    <Card
-      sx={{
-        minWidth: 275,
-        display: 'flex',
-        flexDirection: matches ? 'row' : 'column',
-        alignItems: 'stretch',
-        flexBasis: matches ? 400 : 'auto',
-      }}
-    >
-      <CardMedia
-        sx={{ height: 140, width: matches ? 140 : 'auto' }}
-        image={imageUrl}
-        title={track.album.name}
-      />
-
-      <CardContent sx={{ flex: 1 }}>
-        <TrackInfo
-          trackName={track.name}
-          trackUrl={track.external_urls.spotify}
-          artists={track.artists}
-          album={track.album}
-          durationMs={track.duration_ms}
+    <Card>
+      <Box
+        sx={{
+          minWidth: 275,
+          display: 'flex',
+          flexDirection: matches ? 'row' : 'column',
+          alignItems: 'stretch',
+          flexBasis: matches ? 400 : 'auto',
+        }}
+      >
+        <CardMedia
+          sx={{ height: 140, width: matches ? 140 : 'auto' }}
+          image={imageUrl}
+          title={track.album.name}
         />
-      </CardContent>
+
+        <CardContent sx={{ flex: 1 }}>
+          <TrackInfo
+            trackName={track.name}
+            trackUrl={track.external_urls.spotify}
+            artists={track.artists}
+            album={track.album}
+            durationMs={track.duration_ms}
+          />
+        </CardContent>
+      </Box>
+
+      {track.preview_url && (
+        <Box>
+          <audio controls src={track.preview_url} title={track.name}></audio>
+        </Box>
+      )}
     </Card>
   );
 };
