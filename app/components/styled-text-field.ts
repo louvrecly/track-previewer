4ce4b840ef7354241ab5e4fea309d0e28bef9e46
Louvre Chow <louvrecly@gmail.com>
2023-11-14 15:53:50 -0500

    restyle StyledTextField

--- app/components/styled-text-field.ts
+++ app/components/styled-text-field.ts
@@ -4,9 +4,19 @@ import TextField, { TextFieldProps } from '@mui/material/TextField';
 const StyledTextField = styled(TextField)<TextFieldProps>(({ theme }) => ({
   '& label': {
     color: `${theme.palette.common.white}A`,
+    transform: `translate(${theme.spacing(2)}, ${theme.spacing(1)}) scale(1)`,
+    '&.Mui-focused, &.MuiFormLabel-filled': {
+      transform: `translate(14px, -9px) scale(0.75)`,
+    },
   },
-  '& .MuiOutlinedInput-root fieldset': {
-    borderColor: `${theme.palette.common.white}A`,
+  '& .MuiOutlinedInput-root': {
+    color: theme.palette.common.white,
+    '& input': {
+      padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
+    },
+    '& fieldset': {
+      borderColor: `${theme.palette.common.white}A`,
+    },
   },
   '&:hover': {
     '& label': {
@@ -15,9 +25,6 @@ const StyledTextField = styled(TextField)<TextFieldProps>(({ theme }) => ({
     '& .MuiOutlinedInput-root fieldset': {
       borderColor: theme.palette.common.white,
     },
-  },
-  '& .MuiOutlinedInput-root': {
-    color: theme.palette.common.white,
   },
 }));
 
